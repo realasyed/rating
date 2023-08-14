6858f2ca9cd1ebb88b7aa04ce0fc6f66766e9761
realasyed <armaansyed120@gmail.com>
2023-08-14 12:15:18 -0500

    more preprocessing

--- rating.py
+++ rating.py
@@ -4,5 +4,12 @@ import pandas as pd
 import numpy as np
 
 # Loading and preprocessing data.
-dataset = pd.read_csv("imbd_top_1000.csv")
-subset = dataset[['Series_Title', 'Genre', 'IMBD_Rating', 'Overview', 'Director']]
+dataset = pd.read_csv("imdb_top_1000.csv")
+subset = dataset[['Series_Title', 'Genre', 'IMDB_Rating', 'Overview', 'Director', 'Gross']]
+# st.write(subset)
+
+dep = subset.iloc[['IMDB_Rating', 'Gross']].values
+indep = subset.iloc[['Series_Title', 'Genre', 'Overview', 'Director']].values
+st.write(dep)
+st.write(indep)
+# data is not just numbers so .values not workin idk 
